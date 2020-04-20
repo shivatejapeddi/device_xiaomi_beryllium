5900c82f548c8d1305ca106f4e4eb0f32462e1bd
shivatejapeddi <shivatejapeddi@gmail.com>
2020-04-20 12:57:26 +0530

    beryllium : Remove NoCutoutOverlay *Using Notch City Now

--- device.mk
+++ device.mk
@@ -21,14 +21,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
